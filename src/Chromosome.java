7d72e55eedd00bb7a2153495c05804605e793122
MariamAshrafFekry <mariamashraf096@gmail.com>
2017-10-28 21:08:49 +0200

    Main class

--- src/Chromosome.java
+++ src/Chromosome.java
@@ -6,12 +6,13 @@ public class Chromosome implements Comparable<Chromosome> {
 	public Float fitnessUpperBound;
 
 	public Chromosome(int degree) {
-		genes = new Float[degree];
+		genes = new Float[degree+1];
 	}
 	
 	public Chromosome(Chromosome c) {
 		fitness=c.fitness;
 		fitnessUpperBound=c.fitnessUpperBound;
+		genes=new Float[c.genes.length];
 		for(int i=0;i<genes.length;++i) {
 			genes[i]=c.genes[i];
 		}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String [] args) {
-		// Mariam's Part
+	public static void main(String [] args) throws FileNotFoundException {
+		File file=new File("testcases.txt");
+		Scanner input =new Scanner(file);
+		int testCaseNum=input.nextInt();
+		for(int i=1;i<=testCaseNum;i++)
+		{
+			int numOfPoints=input.nextInt();
+			int degree=input.nextInt();
+			Point points[]=new Point[numOfPoints];
+			for(int j=0;j<numOfPoints;j++)
+			{
+				Point point=new Point();
+				point.x=input.nextFloat();
+				point.y=input.nextFloat();
+				points[j]=point;
+			}
+			System.out.println("case "+i);
+			Float cofficient[]=new Float[degree+1];
+			cofficient=GeneticAlgorithm.run(degree, points);
+			for(int k=0;k<cofficient.length;k++)
+			{
+				System.out.print(cofficient[k]+"\t ");
+			}
+			System.out.println();
+		}
 	}
 }
